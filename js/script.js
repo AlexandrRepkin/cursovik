9610cdc550c300e495a5714e4e6f2adce6d3eaaa
AlexandrRepkin <arepkin96@mail.ru>
2022-07-20 14:26:22 +0300

    14:26

--- js/script.js
+++ js/script.js
@@ -32,6 +32,7 @@ $(document).ready(function() {
             }
         ]
     });
+//==================================Слайдер с отзывами====================================//
     $('.reviews-slider').slick({
         arrows: true,
         slidesToShow: 1,
@@ -57,11 +58,11 @@ $(document).ready(function() {
 //===============================================================================================//
 
 //=========================Плавный переход по якорный ссылкам============================//
-    $('a[href^="#"]').click(function(){ // #1
-        let anchor = $(this).attr('href');  // #2
-        $('html, body').animate({           // #3
-        scrollTop:  $(anchor).offset().top  // #4
-        }, 600);                            // #5
+    $('a[href^="#"]').click(function(){ 
+        let anchor = $(this).attr('href'); 
+        $('html, body').animate({          
+        scrollTop:  $(anchor).offset().top  
+        }, 600);               
         });
 //===============================================================================================//
 });
@@ -69,39 +70,39 @@ $(document).ready(function() {
 
 
 //=========================Анимация текста и картинок============================//
-// const animItems = document.querySelectorAll('.anim-items');
+const animItems = document.querySelectorAll('.anim-items');
 
-//     if(animItems.length >0) {
-//         window.addEventListener('scroll', animOnScroll);
+    if(animItems.length >0) {
+        window.addEventListener('scroll', animOnScroll);
 
-//         function animOnScroll(params){
-//             for (let index = 0; index < animItems.length; index++) {
-//                 const animItem = animItems[index];
-//                 const animItemtHeight = animItem.offsetHeight;
-//                 const animItemOffset = offset(animItem).top;
-//                 const animStart = 4;
+        function animOnScroll(params){
+            for (let index = 0; index < animItems.length; index++) {
+                const animItem = animItems[index];
+                const animItemtHeight = animItem.offsetHeight;
+                const animItemOffset = offset(animItem).top;
+                const animStart = 4;
 
-//                 let animItemPoint = window.innerHeight - animItemtHeight / animStart;
+                let animItemPoint = window.innerHeight - animItemtHeight / animStart;
 
-//                 if (animItemtHeight > window.innerHeight) {
-//                     animItemPoint = window.innerHeight - window.innerHeight / animStart;
-//                 }
+                if (animItemtHeight > window.innerHeight) {
+                    animItemPoint = window.innerHeight - window.innerHeight / animStart;
+                }
 
-//                 if ((scrollY > animItemOffset - animItemPoint) && scrollY < (animItemOffset + animItemtHeight)) {
-//                     animItem.classList.add('active');
-//                 } else {
-//                     if(!animItem.classList.contains('anim-no-hide')){
-//                         animItem.classList.remove('active');
-//                     }
-//                 }
-//             }
-//         }
-//     }
-//     animOnScroll();
-//     function offset(el) {
-//         const rect = el.getBoundingClientRect(),
-//               scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
-//               scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-//         return {top: rect.top + scrollTop, left: rect.left + scrollLeft};
-//     }
+                if ((scrollY > animItemOffset - animItemPoint) && scrollY < (animItemOffset + animItemtHeight)) {
+                    animItem.classList.add('active');
+                } else {
+                    if(!animItem.classList.contains('anim-no-hide')){
+                        animItem.classList.remove('active');
+                    }
+                }
+            }
+        }
+    }
+    animOnScroll();
+    function offset(el) {
+        const rect = el.getBoundingClientRect(),
+              scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+              scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+        return {top: rect.top + scrollTop, left: rect.left + scrollLeft};
+    }
 //===============================================================================================//
